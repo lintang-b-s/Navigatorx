9ae516c037f79e86df0427edc4d91b74909a8f8c
lintangbs <lintangbirdasaputra23@gmail.com>
2026-05-06 00:16:37 +0700

    feat: updated onlineMapMatchJS() return types

--- pkg/datastructure/d_ary_heap.go
+++ pkg/datastructure/d_ary_heap.go
@@ -205,7 +205,7 @@ func (h *DAryHeap[T]) ExtractMin(updatePos func(queryInfoId, newHeapNodeId uint3
 // decreaseKey update rank dari item min-heap.   O(logN) heapify.
 func (h *DAryHeap[T]) DecreaseKey(itemPos uint32, rank float64, updatePos func(queryInfoId, newHeapNodeId uint32)) {
 
-	util.AssertPanic(itemPos < h.Size() && rank <= h.heap[itemPos].GetRank(), fmt.Sprintf("error when decrease key for vertex id = %d", itemPos))
+	util.AssertPanic(itemPos < h.Size() && util.Le(rank, h.heap[itemPos].GetRank()), fmt.Sprintf("error when decrease key for vertex id = %d", itemPos))
 
 	h.heap[itemPos].SetRank(rank)
 	h.heapifyUp(itemPos, updatePos)
