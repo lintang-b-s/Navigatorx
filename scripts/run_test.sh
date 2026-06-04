c817832f4b63b1bd354a477bf0cbd726ba2e0dcb
lintangbs <lintangbirdasaputra23@gmail.com>
2026-06-04 13:58:57 +0700

    chore: updated test.yml github action part 3

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -5,7 +5,10 @@ gdown https://drive.google.com/uc?id=1pRmqUFgNc_p0lEKmfzLcn3IRhUW4Cm4c  --output
 
 REPO_ROOT=$(pwd)
 
-for test_data_dir in tests/*/data; do
+for test_dir in tests/*; do
+  test -d "$test_dir" || continue
+  test_data_dir="$test_dir/data"
+  mkdir -p "$test_data_dir"
   ln -sf "$REPO_ROOT/data/yogyakarta.osm.pbf" "$test_data_dir/yogyakarta.osm.pbf"
   ln -sf "$REPO_ROOT/data/solo.osm.pbf" "$test_data_dir/solo.osm.pbf"
 done
@@ -13,5 +16,3 @@ done
 go test -p 1 ./... -v -timeout=0  -failfast  -count=1 -coverpkg=./pkg/... -coverprofile=cover.out  -skip "^TestOSN2024KRLMALT$|^TestOSN2024KRL$" # skip test yang lama selesainya
 # note: tests harus tidak concurrent (-p 1) karena kita setiap TestFunction pada tests/shortestpath, tests/shortestpath_crp_alt, tests/query write ke file landmark yang sama
 # selain itu kalau kita pakai concurrent, verbose log nya (-v) gak langsung keluar
-
-
