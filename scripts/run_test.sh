8f572808dc9ef7c0ffcaeb95eb86005dfb6218ac
lintangbs <lintangbirdasaputra23@gmail.com>
2026-06-04 13:36:51 +0700

    chore: fix run_test.sh osm.pbf file download

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,7 +1,7 @@
 echo "Running test..."
 
-gdown https://drive.google.com/uc?id=1uBoFWUSRka9pqH2dVPKpcystxXmkkSgs --output ./data
-gdown https://drive.google.com/uc?id=1PjYZk9wl5xSykqjak-ZFvlT427X1eSa0 --output ./data
+gdown https://drive.google.com/uc?id=1HBswl5-JkFXWh--AFLC2ElYC4Tbsj1i0   --output ./data
+gdown https://drive.google.com/uc?id=1pRmqUFgNc_p0lEKmfzLcn3IRhUW4Cm4c  --output ./data
 
 go test -p 1 ./... -v -timeout=0  -failfast  -count=1 -coverpkg=./pkg/... -coverprofile=cover.out  -skip "^TestOSN2024KRLMALT$|^TestOSN2024KRL$" # skip test yang lama selesainya
 # note: tests harus tidak concurrent (-p 1) karena kita setiap TestFunction pada tests/shortestpath, tests/shortestpath_crp_alt, tests/query write ke file landmark yang sama
